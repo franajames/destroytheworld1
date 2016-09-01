1998c874485ad2db28c2a8dd46127f39ef658cb3
Frana <your.email@example.com>
2016-08-31 21:38:02 -0400

    Initial Commit

---
+++ DESTROTHE WORLD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DESTROTHE WORLD
+//
+//  Created by Frana James on 8/31/16.
+//  Copyright © 2016 Frana James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
